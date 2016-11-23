061110c17127238dfcd47528de92b49844285ab4
hugobullont <hugobullont@gmail.com>
2016-11-23 13:55:28 -0500

    Stock! (Servlet)Drinks!

---
+++ src/java/Servlets/UpdateDrinks.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import DataAccess.Products.IProductsRepository;
+import DataAccess.Products.ProductsRepository;
+import Entities.RelRestDrk;
+import Entities.RelRestMak;
+import Entities.Tables;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Usuario
+ */
+@WebServlet(name = "UpdateDrinks", urlPatterns = {"/UpdateDrinks"})
+public class UpdateDrinks extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateDrinks</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateDrinks at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        Tables cUser = (Tables) session.getAttribute("CurrentUser");
+        String[] msu = request.getParameterValues("txtDrinks");
+        IProductsRepository productRepo = new ProductsRepository();
+        List<RelRestDrk> listDrinks = (List<RelRestDrk>) productRepo.GetDrinksByRestaurant(cUser.getRestaurants().getIdRestaurant());
+        for(int i =0;i<listDrinks.size();i++)
+        {
+            RelRestDrk temp = listDrinks.get(i);
+            productRepo.UpdateDrinksStock(cUser.getRestaurants().getIdRestaurant(),Integer.valueOf(msu[i]),temp.getDrinks().getIdDrink());
+        }
+        
+        response.sendRedirect("admin.jsp");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
